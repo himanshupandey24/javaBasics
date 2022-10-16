19b094b770b2dc49c8e920ca8be092bbed1668df
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-10-16 13:24:44 +0530

    A little bit about type erasure

---
+++ src/main/java/org/example/TypeErasure.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class TypeErasure {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hjdh");
+        ArrayList<Object> list2 = list;
+        list2.add(12554);
+}
